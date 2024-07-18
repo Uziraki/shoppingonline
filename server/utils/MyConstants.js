cd784f5fa4732b9640e835b7901ac8409ccf989b
Uziraki <93587368+Uziraki@users.noreply.github.com>
2024-07-18 10:56:53 +0700

    upload sever

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster.wp48aug.mongodb.net',
+  DB_USER: 'MonsterK1901',
+  DB_PASS: 'Khanh1901',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'KHONG_CO_KHOA',
+  JWT_EXPIRES: '31556952000', // in milliseconds (01 year = 31556952000 ms)
+  EMAIL_USER: 'nguyencuukhanh190103@gmail.com', // Gmail service
+  EMAIL_PASS: 'wjhwvjdoislhjbun',
+};
+module.exports = MyConstants;
